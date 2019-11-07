b4bdeab1f3c19a5ec13b373e48bf9613051b3cbe
Eliastion <zajackic@wp.pl>
2019-11-07 06:23:34 +0100

    explicitly marked findQueenBaseMoves as unfinished

--- src/game/moveFinding/findQueenBaseMoves.js
+++ src/game/moveFinding/findQueenBaseMoves.js
@@ -2,5 +2,6 @@ import findBishopBaseMoves from "./findBishopBaseMoves";
 import findRookBaseMoves from "./findRookBaseMoves";
 
 export default function findQueenBaseMoves(boardSituation, startingCoordinates, color) {
+    //TODO: implement this properly once the dependencies are completed.
     return [...findBishopBaseMoves(), ...findRookBaseMoves()];
 }
