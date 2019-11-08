52793675de6c4fa883e3daa767f148d87a2e45dd
Jacek <jkozicki77@gmail.com>
2019-11-08 19:46:05 +0100

    First version - finds every possible move in 8x8 array. Checks for value of 1 to acting as a figure which blocks furhter movement. Returns array of BoardCoordinates with possible moves.

--- src/game/moveFinding/findRookBaseMoves.js
+++ src/game/moveFinding/findRookBaseMoves.js
@@ -1,3 +1,55 @@
-export default function findRookBaseMoves() {
-    return [];
+import BoardCoordinates from "../board/boardCoordinates";
+
+export default function findRookBaseMoves(boardSituation, startingCoordinates, color) {
+    const z= checkMovesHorizontal(boardSituation, startingCoordinates, color); 
+    const p= checkMovesVertical(boardSituation, startingCoordinates, color);
+    const b = [...p, ...z]; //makes one array of possible moves
+    return b;   //returns this array
+}
+
+
+function checkMovesHorizontal (boardSituation, startingCoordinates, color){
+    let x = startingCoordinates.x-1;
+    let movesHorizontal=[];
+    while(x>=0)
+    {
+        if(boardSituation[startingCoordinates.y][x]!=1)
+            movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
+        else
+            break;
+        x--;     
+    }
+    x=startingCoordinates.x+1;
+    while(x<8)
+    {
+        if(boardSituation[startingCoordinates.y][x]!=1)
+            movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
+        else
+            break;
+        x++;
+    }
+    return movesHorizontal;
+}
+
+function checkMovesVertical(boardSituation, startingCoordinates, color){
+    let y=startingCoordinates.y-1;
+    let movesVertical=[];
+    while(y>=0)
+    {
+        if(boardSituation[y][startingCoordinates.x]!=1)
+            movesVertical.push(new BoardCoordinates(startingCoordinates.x, y));
+        else
+            break;
+        y--;
+    }
+    y=startingCoordinates.y+1;
+    while(y<8)
+    {
+        if(boardSituation[y][startingCoordinates.x]!=1)
+            movesVertical.push(new BoardCoordinates (startingCoordinates.x, y));
+        else
+            break;
+        y++;
+    }
+    return movesVertical;
 }
