42391a3d4a0f2b68df38e6972862df10f0e8aa0b
Jacek <jkozicki77@gmail.com>
2019-11-09 17:46:41 +0100

    Update: properly checks for another pieces on chessboard

--- src/game/moveFinding/findRookBaseMoves.js
+++ src/game/moveFinding/findRookBaseMoves.js
@@ -1,54 +1,74 @@
 import BoardCoordinates from "../board/boardCoordinates";
 
 export default function findRookBaseMoves(boardSituation, startingCoordinates, color) {
-    const z= checkMovesHorizontal(boardSituation, startingCoordinates, color); 
-    const p= checkMovesVertical(boardSituation, startingCoordinates, color);
-    const b = [...p, ...z]; //makes one array of possible moves
+    const b = [...checkMovesVertical(boardSituation, startingCoordinates, color), 
+            ...checkMovesHorizontal(boardSituation, startingCoordinates, color)]; //makes one array of possible moves
     return b;   //returns this array
 }
 
 
-function checkMovesHorizontal (boardSituation, startingCoordinates, color){
-    let x = startingCoordinates.x-1;
-    let movesHorizontal=[];
-    while(x>=0)
+function checkMovesHorizontal (boardSituation, startingCoordinates, pieceColor){
+    let x = startingCoordinates.x-1; //x position currently being checked
+    let movesHorizontal=[]; //array of possible moves
+    while(x>=0)  //check current row from piece position to left edge, stop upon finding another piece
     {
-        if(boardSituation[startingCoordinates.y][x]!=1)
+        let posValue = boardSituation[startingCoordinates.y][x];
+        if(posValue == null)
             movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
         else
+        {   //if you can capture piece, it is a possible move
+            if(posValue.color!=pieceColor)
+                movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
             break;
+        }
         x--;     
     }
     x=startingCoordinates.x+1;
-    while(x<8)
+    while(x<8) //check current row from piece position to right edge, stop upon finding another piece
     {
-        if(boardSituation[startingCoordinates.y][x]!=1)
+        let posValue = boardSituation[startingCoordinates.y][x];
+        if( posValue == null)
             movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
         else
+        {
+            if(posValue.color!=pieceColor)
+                movesHorizontal.push(new BoardCoordinates(x,startingCoordinates.y));
             break;
+        }
         x++;
     }
     return movesHorizontal;
 }
 
-function checkMovesVertical(boardSituation, startingCoordinates, color){
+function checkMovesVertical(boardSituation, startingCoordinates, pieceColor){
     let y=startingCoordinates.y-1;
-    let movesVertical=[];
-    while(y>=0)
+    let movesVertical=[]; 
+    
+    while(y>=0) //check current column from piece position to bottom edge, stop upon finding another piece
     {
-        if(boardSituation[y][startingCoordinates.x]!=1)
+        let posValue = boardSituation[y][startingCoordinates.x];
+        if(posValue == null)
             movesVertical.push(new BoardCoordinates(startingCoordinates.x, y));
         else
+        {  
+            if(posValue.color != pieceColor)
+                movesVertical.push(new BoardCoordinates(startingCoordinates.x, y));
             break;
+        }    
         y--;
     }
     y=startingCoordinates.y+1;
-    while(y<8)
+    while(y<8)  //check current column from piece position to top edge, stop upon finding another piece
     {
-        if(boardSituation[y][startingCoordinates.x]!=1)
+        let posValue = boardSituation[y][startingCoordinates.x];
+        if(posValue == null)
             movesVertical.push(new BoardCoordinates (startingCoordinates.x, y));
         else
+        {   
+            if(posValue.color != pieceColor)
+                movesVertical.push(new BoardCoordinates (startingCoordinates.x, y));
             break;
+        }
         y++;
     }
     return movesVertical;
