9028ce3d2dd32aff8dac7c87aee24f9fc9328d88
barbara-droid <57314438+barbara-droid@users.noreply.github.com>
2019-11-10 20:36:23 +0100

    create class ChessPiece

--- src/game/board/chessPiece.js
+++ src/game/board/chessPiece.js
@@ -1,6 +1,30 @@
-export default class ChessPiece {
+class ChessPiece {
     constructor(pieceType, color) {
+        const avalaibleFigures = ChessPiece.getFigureTypes();
+        const avalaibleColors = ChessPiece.getColors();
         this.pieceType = pieceType;
         this.color = color;
+        if (avalaibleColors.indexOf(color) === -1) {
+            throw new Error(
+              "Color bad!!! Could not create figure with color " + color,
+            );
+        }
+        if (avalaibleFigures.indexOf(pieceType) === -1) {
+            throw new Error(
+              "Figure type bad!!! Could not create figure of type " + pieceType,
+            );
+        }
+    }
+
+    static getFigureTypes() {
+        return ["pawn", "king", "queen", "tower", "bishop", "rook"];
+    }
+    
+    static getColors() {
+        return ["white", "black"];
     }
 }
+
+export default ChessPiece;
+
+  
