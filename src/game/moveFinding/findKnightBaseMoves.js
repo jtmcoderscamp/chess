a154f154207508b004efcaf35f1a70f322cb8cd5
brodzik155 <235301@student.pwr.edu.pl>
2019-11-11 14:51:34 +0100

    first commit

---
+++ src/game/moveFinding/findKnightBaseMoves.js
@@ -0,0 +1,14 @@
+function findKnightBaseMoves(){
+    this.colour = color;
+    this.boardPosition = position[x][y];
+    this.possibleMoves = function(){
+        //x+=2, y+=1
+        //x+=2, y-=1
+        //x-=2, y+=1
+        //x-=2, y-=1
+        //x+=1, y+=2
+        //x+=1, y-=2
+        //x+=1, y+=2
+        //x+=1, y+=2
+    }
+}
