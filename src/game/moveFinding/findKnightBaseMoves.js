6daf9cc3ea818f7f32b09e68b35db8713c2a60ed
brodzik155 <235301@student.pwr.edu.pl>
2019-11-11 19:25:45 +0100

    write a funtion looking for knight movement

--- src/game/moveFinding/findKnightBaseMoves.js
+++ src/game/moveFinding/findKnightBaseMoves.js
@@ -1,14 +1,27 @@
-function findKnightBaseMoves(){
-    this.colour = color;
-    this.boardPosition = position[x][y];
-    this.possibleMoves = function(){
-        //x+=2, y+=1
-        //x+=2, y-=1
-        //x-=2, y+=1
-        //x-=2, y-=1
-        //x+=1, y+=2
-        //x+=1, y-=2
-        //x+=1, y+=2
-        //x+=1, y+=2
-    }
+import BoardCoordinates from "../board/boardCoordinates";
+
+var knightMoves = [
+    [-1, -2], [-2, -1], [1, -2], [-2, 1],
+    [2, -1], [-1, 2], [2, 1], [1, 2]
+];
+var outOfBounds = function(i, j) {
+    return ( i < 0 || i >= 8 || j < 0 || j >= 8 );
 }
+
+function findMovesx(boardSituation, startingCoordinates, pieceColor){
+    var movex = startingCoordinates.x;
+    var movey = startingCoordinates.y;
+    var nextMoves = [];
+        for(var move of moves) {
+             var newMovex = movex + move[0];
+             var newMovey = movey + move[1];
+             if( !outOfBounds(newMovex, newMovey)) {
+                        let possiblePosition = boardSituation[newMovex][newMovey];
+                        if(possiblePosition == null && possiblePosition.color != pieceColor){
+                            nextMoves.push([newMovex, newMovey]);
+                   }
+                }
+             }
+        
+        return nextMoves;
+   }
