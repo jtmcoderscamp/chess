697b9458486041e513de3d87e8b93eca723ae6d0
Jacek <jkozicki77@gmail.com>
2019-11-12 17:51:33 +0100

    Now uses a recursive function instead of while loops

--- src/game/moveFinding/findRookBaseMoves.js
+++ src/game/moveFinding/findRookBaseMoves.js
@@ -8,53 +8,46 @@ export default function findRookBaseMoves(boardSituation, startingCoordinates, c
 function checkMoves(currentBoard, startingCoordinates, color)
 {
     let possibleMoves = []; //array with all possible moves
-    //variables used in while loop 
-    let x = startingCoordinates.x+1;
-    let move = true;
-    while(move) //as long as you can move right and push coordinates to array
-    {
-        move=makeStep(currentBoard, x, startingCoordinates.y, color, possibleMoves);
-        x++;
-    }
-
-    x = startingCoordinates.x-1;
-    move = true;
-    while(move)//as long as you can move left and push coordinates to array
-    {
-        move=makeStep(currentBoard,x,startingCoordinates.y, color, possibleMoves);
-        x--;
-    }
-
-    let y = startingCoordinates.y+1;
-    move = true;
-    while(move)
-    {
-        move=makeStep(currentBoard, startingCoordinates.x, y, color, possibleMoves);
-        y++;
-    }
-
-    y = startingCoordinates.y-1;
-    move = true;
-    while(move)
-    {
-        move=makeStep(currentBoard, startingCoordinates.x, y, color, possibleMoves);
-        y--;
-    }
-
+    //call recursive function which checks possible moves in directions:left,right,up,down
+    makeStep(currentBoard, startingCoordinates.x, startingCoordinates.y, color, possibleMoves, "left");
+    makeStep(currentBoard, startingCoordinates.x, startingCoordinates.y, color, possibleMoves, "right");
+    makeStep(currentBoard, startingCoordinates.x, startingCoordinates.y, color, possibleMoves, "up");
+    makeStep(currentBoard, startingCoordinates.x, startingCoordinates.y, color, possibleMoves, "down");
+    //return array with all possible moves
     return possibleMoves;
 }
 
 
-function makeStep(currentBoard, x,y, color, aggregator){
+function makeStep(currentBoard, x, y, color, aggregator, direction){
+    //depending on direction increment or decrement proper variable
+    switch(direction)
+    {
+        case "up":
+            y++;
+            break;
+        case "down":
+            y--;
+            break;
+        case "left":
+            x--;
+            break;
+        case "right":
+            x++;
+            break;
+    }
+    //check if you are still on chessboard
     if(x<0 || x>7 || y<0 || y>7)
         return false;
+    //check for empty place on board
     if (currentBoard[y][x]==null)
     {
         aggregator.push(new BoardCoordinates(x,y));
-        return true;
+        //you haven't found another chess piece - you can move further
+        return makeStep(currentBoard, x, y, color, aggregator, direction);
     }
     else
     {
+        //check if you can capture this piece, if you can it is a legal move
         if(currentBoard[y][x].color!=color)
             aggregator.push(new BoardCoordinates(x,y));
         return false;
