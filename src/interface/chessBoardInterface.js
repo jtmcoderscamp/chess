2e4130cfd491eb1c3ba57d32ac5e605359c66f21
Teresa Skrzypczyk <tereska.skrzypczyk@gmail.com>
2019-11-13 17:41:42 +0100

    chessboard constructor

--- src/interface/chessBoardInterface.js
+++ src/interface/chessBoardInterface.js
@@ -20,32 +20,53 @@ var symbols = {
   "pawnBlack" : "\u265f",
 };
 
-export default class ChessBoardInterface {
-  ChessBoard() {
+class ChessBoardInterface {
+  constructor(parentNode, onFieldClick) {
+    this.parentNode = parentNode;
+    this.onFieldClick = onFieldClick;
+  }
 
+  chessBoard() {
     var board = document.querySelector('#root');
     var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"];    
+    var cells = [];
+
+    function clickElement() {
+    console.log("click");
+    console.log(j);
+    }
 
     for (var i = 0; i < alphabet.length; i++) {
-      var divRow = document.createElement('div');
-          divRow.classList.add("row");
-              for (var j = 8; j >= 1; j--) {
-                  var div = document.createElement('div');
-                  div.setAttribute("data-x", alphabet[i]);
-                  div.setAttribute("data-y", j);
-                  //div.innerText = symbols.bishopBlack;
-          
-              if (i % 2 == j % 2) {
-                div.classList.add("white");
-              } else {
-                div.classList.add("black");
-              }
-              divRow.append(div);
-            }
-            board.append(divRow);
-          } 
+            var divRow = document.createElement('div');
+                divRow.classList.add("row");
+                    for (var j = 8; j >= 1; j--) {
+                        var div = document.createElement('div');
+                        div.setAttribute("data-x", alphabet[i]);
+                        div.setAttribute("data-y", j);
+                        //console.log(this);
+                        //console.log(div.getAttribute("x" + "data-x"));
+                        //console.log(div.getAttribute("y" + "data-y"));
+                        div.addEventListener("click", clickElement);
+                
+                    if (i % 2 == j % 2) {
+                      div.classList.add("white");
+                    } else {
+                      div.classList.add("black");
+                    }
+                    divRow.append(div);
+
+                  }
+                  board.append(divRow);
+                  // board.map(function () {
+                  //   cells.push([this.data("x"), this.data("y")]);
+                  //   return cells
+                  // });
+                  // console.log(cells);
+
+                } 
   };
+  
 }
 
 board = new ChessBoardInterface;
-board.ChessBoard();
+board.chessBoard();
