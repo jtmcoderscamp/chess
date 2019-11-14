eb572f722e84d3bb11228e4faa6993b0f136d474
brodzik155 <235301@student.pwr.edu.pl>
2019-11-14 17:24:23 +0100

    update to class

--- src/game/moveFinding/findKnightBaseMoves.js
+++ src/game/moveFinding/findKnightBaseMoves.js
@@ -1,27 +1,29 @@
 import BoardCoordinates from "../board/boardCoordinates";
+class Knight {
+    
+    outOfBounds(i, j) {
+        return ( i < 0 || i >= 8 || j < 0 || j >= 8 );
+    }
 
-var knightMoves = [
-    [-1, -2], [-2, -1], [1, -2], [-2, 1],
-    [2, -1], [-1, 2], [2, 1], [1, 2]
-];
-var outOfBounds = function(i, j) {
-    return ( i < 0 || i >= 8 || j < 0 || j >= 8 );
-}
-
-function findMovesx(boardSituation, startingCoordinates, pieceColor){
-    var movex = startingCoordinates.x;
-    var movey = startingCoordinates.y;
-    var nextMoves = [];
-        for(var move of moves) {
-             var newMovex = movex + move[0];
-             var newMovey = movey + move[1];
-             if( !outOfBounds(newMovex, newMovey)) {
-                        let possiblePosition = boardSituation[newMovex][newMovey];
-                        if(possiblePosition == null && possiblePosition.color != pieceColor){
-                            nextMoves.push([newMovex, newMovey]);
-                   }
+    findMovesx(boardSituation, startingCoordinates, pieceColor){
+        const knightMoves = [
+            [-1, -2], [-2, -1], [1, -2], [-2, 1],
+            [2, -1], [-1, 2], [2, 1], [1, 2]
+        ];
+        var moveX = startingCoordinates.x;
+        var moveY = startingCoordinates.y;
+        var nextMoves = [];
+            for(var move of knightMoves) {
+                var newMoveX = moveX + move[0];
+                var newMoveY = movey + move[1];
+                if( !outOfBounds(newMoveX, newMoveY)) {
+                            let possiblePosition = boardSituation[newMovex][newMovey];
+                            if(possiblePosition == null || possiblePosition.color != pieceColor){
+                                nextMoves.push([newMoveX, newMoveX]);
+                    }
+                    }
                 }
-             }
-        
-        return nextMoves;
-   }
+            
+            return nextMoves;
+    }
+}
