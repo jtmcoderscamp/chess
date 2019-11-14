4090df7ab07f1fd65edb8384293fe32a91de5c8e
Joanna <joantymkow@gmail.com>
2019-11-14 17:48:05 +0100

    definition of pawn moves and function -moves out of the board

---
+++ src/game/moveFinding/findPawnBaseMoves.js
@@ -0,0 +1,27 @@
+import BoardCoordinates from "../board/boardCoordinates";
+
+//definition of pawn moves
+var pawnBaseMoves = [[1, 0], [2, 0]];
+
+//definition of board size and moves out of the board - if value=false => move out of board
+var outOfBoard = function(a, b) {
+    return( a<0 || a>= 8 || b<0 || b>=8 );
+}
+
+//definition of finding possible moves for pawn
+function movesFindingX(boardSituation, startingCoordinates, color) {
+    var moveX = startingCoordinates.x;
+    var moveY = startingCoordinates.y;
+    var move //change of position
+    var nextPossibleMoves = [];
+        for (){
+
+        }
+        return nextPossibleMoves;
+}
+/*
+export default function findQueenBaseMoves(boardSituation, startingCoordinates, color) {
+    //TODO: implement this properly once the dependencies are completed.
+    return [...findBishopBaseMoves(), ...findRookBaseMoves()];
+} 
+*/
