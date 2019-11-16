f2a6e80f866ea562b2db3d3eb06eec5f228748de
Magdalena Dyktyńska <magdalena.dyktyska@gmail.com>
2019-11-16 12:36:56 +0100

    changed  get position()

--- src/game/board/boardSituation.js
+++ src/game/board/boardSituation.js
@@ -21,9 +21,9 @@ export default class BoardSituation {
         }
      }
 
-    get situation() {
+    get position() {
             // TODO: this function should return current position on the board as an 8x8 array of ChessPiece objects
-            return this._actualPosition;
+            return this._actualPosition.position;
     }
     get previousSituation() {
             // TODO: this function should return previous situation or null if there's none (in case of the first position)
