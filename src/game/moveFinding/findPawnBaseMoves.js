29b9aa16161f71e1c01efd8df596fc4f5745d017
Joanna <joantymkow@gmail.com>
2019-11-16 22:19:55 +0100

    Forward moves corrected, diagonal moves added

--- src/game/moveFinding/findPawnBaseMoves.js
+++ src/game/moveFinding/findPawnBaseMoves.js
@@ -7,29 +7,55 @@ export default function findPawnBaseMoves(boardSituation, startingCoordinates, c
 
 //definition of pawn moves
 
-
 function findMoves(actualBoard, startingCoordinates, color) {
     let possibleMoves = []; //Output array
     let actual = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y);
-    let deltax = [0, 1];
-    let deltay = [1, 2];
-    //checking move 2 fields forward
-    if ((startingCoordinates.y=2 || startingCoordinates.y=7) && (startingCoordinates.y + 1 == null)) && (startingCoordinates.y + 2 == null) {
-        startingCoordinates.x + deltax[0];
-        startingCoordinates.y + deltay[1];
-        possibleMoves.push(actual);
-    }
-    //checking move 1 field forward
-    else if (startingCoordinates.y + 1 == null) {
-        startingCoordinates.x + deltax[0];
-        startingCoordinates.y + deltay[0];
-        possibleMoves.push(actual);
-    }
-    else if () {
+    const forwardDirection;
+        color === 'white' ? forwardDirection = 1 : forwardDirection = -1;
 
+    //checking move 2 fields forward
+    const oneForwardTargetCoords = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y + forwardDirection);
+    const twoForwardTargetCoords = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y + 2*forwardDirection);
+    const pawnInStartingPosition = function(twoForwardTargetCoords, color) {
+        if ((color === 'white' && startingCoordinates.y === 1) || (color === 'black' && startingCoordinates.y === 6)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    if (pawnInStartingPosition === true && oneForwardTargetCoords == null && twoForwardTargetCoords == null ) {
+        possibleMoves.push(twoForwardTargetCoords);
     }
     else {
-
+        console.log('illegal move');
     }
+
+    //checking move 1 field forward
+    if (oneForwardTargetCoords == null) {
+        possibleMoves.push(oneForwardTargetCoords);
+    }
+    else {
+        console.log('illegal move');
+    }
+
+    //checking beating diagonal left
+    const diagonalLeft = new BoardCoordinates(startingCoordinates.x - forwardDirection, startingCoordinates.y + forwardDirection);
+    if (diagonalLeft !== null) {
+        possibleMoves.push(diagonalLeft);
+    }
+    else {
+        console.log('illegal move');
+    }
+
+    //checking beating diagonal right
+    const diagonalRight = new BoardCoordinates(startingCoordinates.x + forwardDirection, startingCoordinates.y + forwardDirection);
+    if (diagonalRight !== null) {
+        possibleMoves.push(diagonalRight);
+    }
+    else {
+        console.log('illegal move');
+    }
+    
     return possibleMoves;
 }
