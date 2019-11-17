19b0105e4bb7d7ccece834e26e747e426f8c4fe8
Joanna <joantymkow@gmail.com>
2019-11-17 20:03:31 +0100

    Code corrected, conditions of moves forward merged in one block

--- src/game/moveFinding/findPawnBaseMoves.js
+++ src/game/moveFinding/findPawnBaseMoves.js
@@ -4,57 +4,40 @@ export default function findPawnBaseMoves(boardSituation, startingCoordinates, c
     let actualBoard = boardSituation.position;
     return findMoves(currentBoard, startingCoordinates, color);
 }
+//function which cheks if it's the first move of pawn in the game
+
+const pawnInStartingPosition = function(startingCoordinates, color) {
+    return ((color === 'white' && startingCoordinates.y === 1) || (color === 'black' && startingCoordinates.y === 6))
+}
 
 //definition of pawn moves
 
 function findMoves(actualBoard, startingCoordinates, color) {
     let possibleMoves = []; //Output array
     let actual = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y);
-    const forwardDirection;
-        color === 'white' ? forwardDirection = 1 : forwardDirection = -1;
+    const forwardDirection = color === 'white' ? 1 : -1;
 
-    //checking move 2 fields forward
+    //checking move 1 and 2 fields forward
     const oneForwardTargetCoords = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y + forwardDirection);
     const twoForwardTargetCoords = new BoardCoordinates(startingCoordinates.x, startingCoordinates.y + 2*forwardDirection);
-    const pawnInStartingPosition = function(twoForwardTargetCoords, color) {
-        if ((color === 'white' && startingCoordinates.y === 1) || (color === 'black' && startingCoordinates.y === 6)) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    if (pawnInStartingPosition === true && oneForwardTargetCoords == null && twoForwardTargetCoords == null ) {
-        possibleMoves.push(twoForwardTargetCoords);
-    }
-    else {
-        console.log('illegal move');
-    }
-
-    //checking move 1 field forward
-    if (oneForwardTargetCoords == null) {
+    
+    if (actualBoard[oneForwardTargetCoords.x][oneForwardTargetCoords.y] == null) {
         possibleMoves.push(oneForwardTargetCoords);
     }
-    else {
-        console.log('illegal move');
-    }
+        if (pawnInStartingPosition(startingCoordinates, color) === true && actualBoard[oneForwardTargetCoords.x][oneForwardTargetCoords.y] == null && actualBoard[twoForwardTargetCoords.x][twoForwardTargetCoords.y] == null ) {
+            possibleMoves.push(twoForwardTargetCoords);
+    }   
 
     //checking beating diagonal left
-    const diagonalLeftTargetCoords = new BoardCoordinates(startingCoordinates.x - forwardDirection, startingCoordinates.y + forwardDirection);
-    if (diagonalLeftTargetCoords !== null && actualBoard[diagonalLeftTargetCoords].color !== color) {
+    const diagonalLeftTargetCoords = new BoardCoordinates(startingCoordinates.x - 1, startingCoordinates.y + forwardDirection); //in case of y coordinate the use of forwardDirection is still necessary, right?
+    if (actualBoard[diagonalLeftTargetCoords.x][diagonalLeftTargetCoords.y] == null && actualBoard[diagonalLeftTargetCoords.x][diagonalLeftTargetCoords.y].color !== color) {
         possibleMoves.push(diagonalLeftTargetCoords);
-    }
-    else {
-        console.log('illegal move');
     }
 
     //checking beating diagonal right
     const diagonalRightTargetCoords = new BoardCoordinates(startingCoordinates.x + forwardDirection, startingCoordinates.y + forwardDirection);
-    if (diagonalRightTargetCoords !== null && actualBoard[diagonalRightTargetCoords].color !== color) {
-        possibleMoves.push(diagonalRightTargetCoords;
-    }
-    else {
-        console.log('illegal move');
+    if (actualBoard[diagonalRightTargetCoords.x][diagonalRightTargetCoords.y] == null && actualBoard[diagonalRightTargetCoords.x][diagonalRightTargetCoords.y].color !== color) {
+        possibleMoves.push(diagonalRightTargetCoords);
     }
     
     return possibleMoves;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import MoveFinder from "./game/moveFinding/moveFinder";
 import MockBoardInterface from "./interface/mockBoardInterface";
 import Game from "./game";
 import findRookBaseMoves from "./game/moveFinding/findRookBaseMoves";
+import findPawnBaseMoves from "./game/moveFinding/findPawnBaseMoves";
+import ChessPiece from "./game/board/chessPiece";
 
 /**
  * This file initializes the application
@@ -12,7 +14,10 @@ console.log("...");
 
 let moveFinder = new MoveFinder();
 
-moveFinder.registerAlgorithm("pawn", findRookBaseMoves);
+moveFinder.registerAlgorithm("pawn", findPawnBaseMoves);
 moveFinder.registerAlgorithm("rook", findRookBaseMoves);
 
 const game = new Game(document.querySelector('#root'), MockBoardInterface, moveFinder, true);
+
+game._currentSituation._actualPosition.position[5][2] = new ChessPiece("pawn","black");
+game._display.setPosition(game._currentSituation._actualPosition.position);
