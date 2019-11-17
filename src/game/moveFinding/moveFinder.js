2f011a9668a9d7cdf73a294fba936515c74d3588
jtmaluszynski <j.t.maluszynski@gmail.com>
2019-11-17 08:15:45 +0100

    fixed leftover code from bad testing

--- src/game/moveFinding/moveFinder.js
+++ src/game/moveFinding/moveFinder.js
@@ -8,7 +8,7 @@ export default class MoveFinder {
     }
 
     findMoves(pieceType, situation, coordinates, pieceColor) {
-        const aggregator = [{x: 5, y:6}];
+        const aggregator = [];
 
         //pick appropriate algorithm group
         const algorithms = this._registeredMoveFindingAlgorithms[pieceType];
@@ -16,12 +16,12 @@ export default class MoveFinder {
 
         //iterate through algorithms, adding found moves from each of them
         if (algorithms.length) {
-            for (let i = 0; i < algorithms.length ; i++) {
+            for (let i = 0; i < algorithms.length; i++) {
                 let moves = algorithms[i](situation, coordinates, pieceColor);
                 aggregator.push(...moves);
             }
         }
-        
+
         //remove duplicates
         this._makeUnique(aggregator);
 
@@ -39,7 +39,7 @@ export default class MoveFinder {
     _makeUnique(coordsArray) {
         for (let i = 0; i < coordsArray.length; i++) {
             for (let j = i + 1; j < coordsArray.length; j++) {
-                if (coordsArray[i].x === coordsArray[j].x && coordsArray[i].y === coordsArray[j].y){
+                if (coordsArray[i].x === coordsArray[j].x && coordsArray[i].y === coordsArray[j].y) {
                     coordsArray.splice(j--, 1);
                 }
             }
